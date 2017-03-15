986551b1f5c6552e216c4becbb61566b5076fd0e
Marcel Geibel <marcel-geibel@hotmail.de>
2017-03-15 14:49:21 +0100

    Klassen Stick, Block, Ball, ItemFactory erstellt

---
+++ Breakout/src/de/tudarmstadt/informatik/fop/breakout/entities/Ball.java
@@ -0,0 +1,21 @@
+package de.tudarmstadt.informatik.fop.breakout.entities;
+
+import org.newdawn.slick.geom.Vector2f;
+
+import eea.engine.entity.Entity;
+import eea.engine.interfaces.IMovement;
+
+public class Ball extends Entity implements IMovement {
+
+	public Ball(String entityID) {
+		super(entityID);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Vector2f getNextPosition(Vector2f arg0, float arg1, float arg2, int arg3) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
