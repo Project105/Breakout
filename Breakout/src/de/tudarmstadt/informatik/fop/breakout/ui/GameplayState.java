8ef1de365453f35aa5b909a46791ad71bad6f749
Denis <Lilka@130.83.116.180>
2017-03-15 15:29:53 +0100

    comments and format

--- Breakout/src/de/tudarmstadt/informatik/fop/breakout/ui/GameplayState.java
+++ Breakout/src/de/tudarmstadt/informatik/fop/breakout/ui/GameplayState.java
@@ -4,7 +4,6 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
-import org.newdawn.slick.KeyListener;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.state.BasicGameState;
@@ -19,45 +18,52 @@ import eea.engine.event.basicevents.KeyPressedEvent;
 
 public class GameplayState extends BasicGameState implements GameParameters {
 	private int idState;
-	private  StateBasedEntityManager entityManager;
-	
-	public GameplayState(int ID){
-		idState=ID;
+	private StateBasedEntityManager entityManager;
+
+	public GameplayState(int ID) {
+		idState = ID;
 		entityManager = StateBasedEntityManager.getInstance();
 	}
 
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-		
-		
-		//Setting up background entity
-		Entity background= new Entity(BACKGROUND_ID);
-		//setting up position
-		background.setPosition(new Vector2f(WINDOW_WIDTH/2,WINDOW_HEIGHT/2));
-		//adding image to entity
+
+		// Setting up background entity
+		Entity background = new Entity(BACKGROUND_ID);
+		// setting up position
+		background.setPosition(new Vector2f(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2));
+		// adding image to entity
 		background.addComponent(new ImageRenderComponent(new Image(BACKGROUND_IMAGE)));
-		//adding entity to entityManager
+		// adding entity to entityManager
 		entityManager.addEntity(idState, background);
 		
+		
+		/*
+		 * Escape function entity
+		 */
+		// Setting up entity
 		Entity escListener = new Entity("ESC_Listener");
-		KeyPressedEvent esc=new KeyPressedEvent(Input.KEY_ESCAPE);
+		// Event if is Escape pressed
+		KeyPressedEvent esc = new KeyPressedEvent(Input.KEY_ESCAPE);
+		// Action switch states if esc pressed
 		esc.addAction(new ChangeStateAction(MAINMENU_STATE));
+		// adding EventComponent in entity
 		escListener.addComponent(esc);
+		// adding entity in entityManager
 		entityManager.addEntity(idState, escListener);
 	}
+
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
 		entityManager.updateEntities(gc, sbg, delta);
-		
+
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		entityManager.renderEntities(gc, sbg, g);
-		
-	}
 
-	
+	}
 
 	@Override
 	public int getID() {
