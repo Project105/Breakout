0061188d4dd9f6ad3540877f4c094a9a466b0a69
Lilka <Lilka@130.83.116.180>
2017-03-15 13:01:54 +0100

    Background and Button

--- Breakout/src/de/tudarmstadt/informatik/fop/breakout/ui/MainMenuState.java
+++ Breakout/src/de/tudarmstadt/informatik/fop/breakout/ui/MainMenuState.java
@@ -1,5 +1,65 @@
 package de.tudarmstadt.informatik.fop.breakout.ui;
 
-public class MainMenuState {
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Vector2f;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+import de.tudarmstadt.informatik.fop.breakout.constants.GameParameters;
+import eea.engine.component.render.ImageRenderComponent;
+import eea.engine.entity.Entity;
+import eea.engine.entity.StateBasedEntityManager;
+/*
+ * @Author Denis Andric
+ */
+public class MainMenuState extends BasicGameState implements GameParameters {
+
+	private int idState;
+	private StateBasedEntityManager entityManager;
+	
+	public MainMenuState(int id){
+		idState=id;
+		entityManager=StateBasedEntityManager.getInstance();
+	}
+	
+	@Override
+	public void init(GameContainer sc, StateBasedGame sb) throws SlickException {
+		//Background Entity
+		Entity background=new Entity("menu");
+		background.setPosition(new Vector2f(WINDOW_WIDTH/2,WINDOW_HEIGHT/2));
+		background.addComponent(new ImageRenderComponent(new Image(BACK_IMAGE)));
+		entityManager.addEntity(MAINMENU_STATE, background);
+		
+		
+		//Buttons
+		Entity nGame =new Entity("New Game");
+		nGame.setPosition(new Vector2f(218,190));
+		
+		nGame.addComponent(new ImageRenderComponent(new Image(ENTRY_IMAGE)));
+		entityManager.addEntity(MAINMENU_STATE, nGame);
+	}
+
+	
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sb, int delta) throws SlickException {
+		entityManager.updateEntities(gc, sb, delta);
+		
+	}
+	
+	@Override
+	public void render(GameContainer gc, StateBasedGame sb, Graphics g) throws SlickException {
+		entityManager.renderEntities(gc, sb, g);
+		
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return idState;
+	}
 
 }
