948f534468aded0a44d33e4125ec986ab3d21fc8
Denis <Lilka@idea-PC>
2017-03-16 12:36:25 +0100

    Improving

---
+++ Breakout/src/de/tudarmstadt/informatik/fop/breakout/entities/Stick.java
@@ -0,0 +1,27 @@
+package de.tudarmstadt.informatik.fop.breakout.entities;
+
+import eea.engine.entity.Entity;
+import eea.engine.interfaces.ICollidable;
+
+public class Stick extends Entity implements ICollidable {
+	
+	
+	
+	
+	public Stick(String entityID) {
+		super(entityID);
+		setPassable(false);
+		setSize(null);
+		setVisible(true);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+	@Override
+	public boolean collides(float arg0, float arg1) {
+		if(arg0==this.getPosition().getX()&&arg1==this.getPosition().getY())return true;
+		else return false;
+	}
+
+}
